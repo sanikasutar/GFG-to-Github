0f925f6105095e9b911417cf2044c18579083147
Sanika_Sutar <114049517+sanikasutar@users.noreply.github.com>
2024-07-13 21:23:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Common in 3 Sorted Arrays/common-in-3-sorted-arrays.cpp
@@ -0,0 +1,78 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function to find common elements in three arrays.
+      vector<int> commonElements(vector<int> &a, vector<int> &b, vector<int> &c) {
+    vector<int> ans;
+    long long n = a.size();
+    long long m = b.size();
+    long long l = c.size();
+    long long i = 0, j = 0, k = 0;
+    
+    while ((i < n) && (j < m) && (k < l)) {
+        if (a[i] == b[j] && a[i] == c[k]) {
+            // Avoid duplicates in the result vector
+            if (ans.empty() || ans.back() != a[i]) {
+                ans.push_back(a[i]);
+            }
+            i++;
+            j++;
+            k++;
+        } else if (a[i] < b[j]) {
+            i++;
+        } else if (b[j] < c[k]) {
+            j++;
+        } else {
+            k++;
+        }
+    }
+    
+    return ans;
+}
+
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr, brr, crr;
+        string input1;
+        getline(cin, input1);
+        stringstream ss1(input1);
+        int number1;
+        while (ss1 >> number1) {
+            arr.push_back(number1);
+        }
+        string input2;
+        getline(cin, input2);
+        stringstream ss2(input2);
+        int number2;
+        while (ss2 >> number2) {
+            brr.push_back(number2);
+        }
+        string input3;
+        getline(cin, input3);
+        stringstream ss3(input3);
+        int number3;
+        while (ss3 >> number3) {
+            crr.push_back(number3);
+        }
+        Solution ob;
+        vector<int> res = ob.commonElements(arr, brr, crr);
+        if (res.size() == 0)
+            cout << -1;
+        for (int i = 0; i < res.size(); i++)
+            cout << res[i] << " ";
+        cout << endl;
+    }
+}
+// } Driver Code Ends
