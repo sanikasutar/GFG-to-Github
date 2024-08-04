58fa2c3fb7efdb38c82ae75320effd4ded5ec415
Sanika_Sutar <114049517+sanikasutar@users.noreply.github.com>
2024-08-04 20:47:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: School/Pattern of Strings/pattern-of-strings.cpp
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	
+	vector<string> pattern(string S)
+	{
+	   vector<string> ans;
+	   int i = 0;
+	   int j = S.size();
+	   string str = "";
+	   while(i != j)
+	   {
+	       for(auto c = i; c < j; c++)
+	       {
+	           str+=S[c];
+	       }
+	       ans.push_back(str);
+	       j--;
+	       str="";
+	   }
+	   return ans;
+	}
+
+};
+
+//{ Driver Code Starts.
+
+int main() 
+{
+   	
+   	ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+   
+   	int t;
+   	string tc;
+   	getline(cin, tc);
+   	t = stoi(tc);
+   	while(t--)
+   	{
+   		string s;
+   		getline(cin, s);
+
+   		Solution ob;
+
+   		vector<string> ans = ob.pattern(s);
+
+   		for(int i = 0; i < ans.size(); i++)
+   			cout << ans[i] << "\n";
+   		
+   	}
+
+    return 0;
+}
+// } Driver Code Ends
